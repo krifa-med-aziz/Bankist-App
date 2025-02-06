95e8c64213adfa91645442aff3feaf3f782adbc4
Krifa Med Aziz <87083945+krifaaziz@users.noreply.github.com>
2025-02-06 01:12:03 +0100

    Refactor movement dates handling and improve date formatting logic

--- script.js
+++ script.js
@@ -9,16 +9,6 @@ const account1 = {
   interestRate: 1.2, // %
   pin: 1111,
   movementsDates: [
-    '2024-11-18T21:31:17.178Z',
-    '2024-12-23T07:42:02.383Z',
-    '2025-01-28T09:15:04.904Z',
-    '2025-04-01T10:17:24.185Z',
-    '2025-02-08T14:11:59.604Z',
-    '2025-03-26T17:01:17.194Z',
-    '2025-03-28T23:36:17.929Z',
-    '2025-05-01T10:51:36.790Z',
-  ],
-  movementsDates: [
     '2024-02-02T10:51:36.790Z',
     '2024-07-28T23:36:17.929Z',
     '2024-08-01T10:17:24.185Z',
@@ -35,16 +25,6 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
-  movementsDates: [
-    '2024-12-18T21:31:17.178Z',
-    '2024-11-18T07:42:02.383Z',
-    '2025-02-15T09:15:04.904Z',
-    '2025-03-02T10:17:24.185Z',
-    '2025-01-09T14:11:59.604Z',
-    '2025-02-24T17:01:17.194Z',
-    '2025-05-20T23:36:17.929Z',
-    '2025-04-01T10:51:36.790Z',
-  ],
   movementsDates: [
     '2024-02-02T10:51:36.790Z',
     '2024-07-28T23:36:17.929Z',
@@ -72,16 +52,6 @@ const account3 = {
     '2025-03-28T23:36:17.929Z',
     '2025-05-01T10:51:36.790Z',
   ],
-  movementsDates: [
-    '2024-02-02T10:51:36.790Z',
-    '2024-07-28T23:36:17.929Z',
-    '2024-08-01T10:17:24.185Z',
-    '2024-09-28T09:15:04.904Z',
-    '2024-11-18T21:31:17.178Z',
-    '2024-12-23T07:42:02.383Z',
-    '2025-02-02T17:01:17.194Z',
-    '2025-02-04T14:11:59.604Z',
-  ],
 };
 
 const account4 = {
@@ -95,13 +65,6 @@ const account4 = {
     '2025-03-02T10:17:24.185Z',
     '2025-02-24T17:01:17.194Z',
     '2025-04-01T10:51:36.790Z',
-  ],
-  movementsDates: [
-    '2024-02-15T09:15:04.904Z',
-    '2024-12-18T21:31:17.178Z',
-    '2025-01-24T17:01:17.194Z',
-    '2025-02-02T17:01:17.194Z',
-    '2025-02-04T14:11:59.604Z',
   ],
 };
 
@@ -133,31 +96,45 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const now = new Date();
-const day = `${now.getDay()}`.padStart(2, 0);
-const month = `${now.getMonth()}`.padStart(2, 0);
-const year = now.getFullYear();
-const heure = now.getHours();
-const minute = `${now.getMinutes()}`.padStart(2, 0);
-labelDate.textContent = `${month}/${day}/${year} ${heure}:${minute} `;
+const local = navigator.language;
+const options = {
+  hour: 'numeric',
+  minute: 'numeric',
+  day: 'numeric',
+  month: 'numeric',
+  year: 'numeric',
+  // weekday: 'long',
+};
+
+const formatMovementDate = function (date) {
+  const calcdaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+  const dayPassed = calcdaysPassed(new Date(), date);
+  if (dayPassed === 0) return 'Today';
+  else if (dayPassed === 1) return 'Yesterday';
+  else if (dayPassed <= 7) return `${dayPassed} Days Ago`;
+  else return new Intl.DateTimeFormat(local).format(date);
+};
 
 const displayMouvements = function (acc, sort = false) {
-  const movs = sort
-    ? acc.movements.slice().sort((a, b) => a - b)
-    : acc.movements;
   containerMovements.innerHTML = '';
-  movs.forEach(function (mov, i) {
-    const date = new Date(acc.movementsDates[i]);
-    const displayDate = formatMovementDate(date);
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
+  const combinedMovsDates = acc.movements.map((mov, i) => ({
+    movement: mov,
+    movDate: new Date(acc.movementsDates.at(i)),
+  }));
 
+  if (sort) combinedMovsDates.sort((a, b) => a.movement - b.movement);
+  combinedMovsDates.forEach(function (obj, i) {
+    const { movement, movDate } = obj;
+    const date = formatMovementDate(movDate);
+    const type = movement < 0 ? 'withdrawal' : 'deposit';
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__date">${displayDate}</div>
-        <div class="movements__value">${mov.toFixed(2)}€</div>
+        <div class="movements__date">${date}</div>
+        <div class="movements__value">${movement.toFixed(2)}DT</div>
       </div>
     `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -185,14 +162,10 @@ const calculDisplaySummary = function (account) {
   const summaryValueIn = account.movements
     .filter(mov => mov > 0)
     .reduce((acc, curr) => acc + curr, 0)
-    .toFixed(2)
-    .reduce((acc, curr) => acc + curr, 0)
     .toFixed(2);
   labelSumIn.textContent = `${summaryValueIn}DT`;
   const summaryValueOut = account.movements
     .filter(mov => mov < 0)
-    .reduce((acc, mov) => acc + mov, 0)
-    .toFixed(2)
     .reduce((acc, mov) => acc + mov, 0)
     .toFixed(2);
   labelSumOut.textContent = `${Math.abs(summaryValueOut)}DT`;
@@ -207,7 +180,6 @@ const calculDisplaySummary = function (account) {
 const updateUI = acc => {
   calcDisplayBalance(acc);
   calculDisplaySummary(acc);
-  displayMouvements(acc);
   displayMouvements(acc);
 };
 
