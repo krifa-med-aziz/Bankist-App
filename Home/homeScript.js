79616bb513b057f6764505a8f73e33354bec37da
Krifa Med Aziz <87083945+krifaaziz@users.noreply.github.com>
2025-02-06 16:10:43 +0100

    Add modal functionality and rename account HTML file; update styles and scripts

---
+++ Home/homeScript.js
@@ -0,0 +1,7 @@
+'use strict';
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
