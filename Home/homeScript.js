dfce731abc5c6846c690960087734ad23fdf5b16
Krifa Med Aziz <87083945+krifaaziz@users.noreply.github.com>
2025-02-08 20:56:08 +0100

    Add scroll-to-top button and enhance navigation functionality

--- Home/homeScript.js
+++ Home/homeScript.js
@@ -5,7 +5,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = () => {
+const openModal = e => {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -23,3 +24,37 @@ document.addEventListener('keydown', e => {
     closeModal();
   }
 });
+////////////////////////
+// Scrolling btn
+const scrollUp = document.querySelector('.scrollToTop');
+const btnLearnMore = document.querySelector('.btn--scroll-to');
+const nav = document.querySelector('.nav');
+btnLearnMore.addEventListener('click', () =>
+  document.querySelector('#section--1').scrollIntoView({ behavior: 'smooth' })
+);
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 700) {
+    scrollUp.classList.remove('hidden');
+    nav.classList.add('sticky');
+  } else {
+    scrollUp.classList.add('hidden');
+    nav.classList.remove('sticky');
+  }
+});
+scrollUp.addEventListener('click', e => {
+  e.preventDefault();
+  document.querySelector('.header').scrollIntoView({ behavior: 'smooth' });
+});
+////////////////////////
+// page navigation
+const links = document.querySelectorAll('.nav__link');
+links.forEach(el => {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (e.target.classList.contains('nav__link')) {
+      const id = this.getAttribute('href');
+      document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+////////////////////////
